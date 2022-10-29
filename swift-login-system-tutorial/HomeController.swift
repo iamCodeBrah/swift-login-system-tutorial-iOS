7c373a23a78dd9ee88c71e9f67f0d2e9db6b531b
iamCodeBrah <letscodebrah@gmail.com>
2022-10-29 14:47:32 -0700

    Lesson 1 - Project setup

---
+++ swift-login-system-tutorial/HomeController.swift
@@ -0,0 +1,16 @@
+//
+//  HomeController.swift
+//  swift-login-system-tutorial
+//
+//  Created by YouTube on 2022-10-26.
+//
+
+import UIKit
+
+class HomeController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .systemPurple
+    }
+}
